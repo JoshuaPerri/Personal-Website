15359b9c6cc574d007121ac3703bdcd232f57665
JoshuaPerri <54598432+JoshuaPerri@users.noreply.github.com>
2021-11-21 16:32:28 -0500

    Preliminary work on resume page

--- js/nav.js
+++ js/nav.js
@@ -1,17 +1,13 @@
-document.getElementById('nav-grow').addEventListener('click', (e) => {
-  var navWindow = document.getElementById('nav-window');
-  var navGrow = document.getElementById('nav-grow');
-  if (navWindow.classList.contains('closed')) {
-    navWindow.classList.add('open');
-    navWindow.classList.remove('closed');
-    navGrow.classList.add('open');
-    navGrow.classList.remove('closed');
-    navGrow.textContent = '<';
+document.getElementById("nav-grow").addEventListener("click", (e) => {
+  var navWindow = document.getElementById("nav-window");
+  var navGrow = document.getElementById("nav-grow");
+  if (!navWindow.classList.contains("open")) {
+    navWindow.classList.add("open");
+    navGrow.classList.add("open");
+    navGrow.textContent = "<";
   } else {
-    navWindow.classList.add('closed');
-    navWindow.classList.remove('open');
-    navGrow.classList.add('closed');
-    navGrow.classList.remove('open');
-    navGrow.textContent = '>';
+    navWindow.classList.remove("open");
+    navGrow.classList.remove("open");
+    navGrow.textContent = ">";
   }
 });
